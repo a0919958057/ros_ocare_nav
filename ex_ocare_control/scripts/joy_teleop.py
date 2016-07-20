e6e48e0c5efbe8ffbf8d57a7fc63eff73945bac7
TaiwanET <a0919958057@gmail.com>
2016-07-20 23:11:35 +0800

    修正Joy控制節點為累加型式

--- ex_ocare_control/scripts/joy_teleop.py
+++ ex_ocare_control/scripts/joy_teleop.py
@@ -2,29 +2,45 @@
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
- 
+
+linear_speed = 0.0
+angular_speed = 0.0
+angular_speed_micro = 0.0
+angular_offset = 0.0
+orient = 0.0
+
 def callback(data):
-	twist = Twist()
-	if  abs(data.axes[1]) > 0.05 :
-		twist.linear.x = (data.axes[1]) * 100
-		
-	if abs(data.axes[2]) > 0.01:
-		twist.angular.z = 3.1415926*(data.axes[2]) /2
-		
-	pub.publish(twist)
- 
+    global angular_speed
+    global angular_speed_micro
+    global linear_speed
+    global angular_offset
+
+    linear_speed = data.axes[1] * 100
+    angular_speed = data.axes[2]*0.01
+    angular_speed_micro = data.axes[0]*0.003
+    angular_offset = data.axes[3]*0.5
+
 def joy_teleop():
-	rospy.init_node('Joy_teleop')
-	rospy.Subscriber("joy", Joy, callback)
-	global pub
-	pub = rospy.Publisher('/ocare/pose_fuzzy_controller/diff_cmd', Twist)
+    global angular_speed
+    global angular_speed_micro
+    global linear_speed
+    global angular_offset
+    global orient
+    rospy.init_node('Joy_teleop')
+    rospy.Subscriber("joy", Joy, callback)
+    global pub
+    pub = rospy.Publisher('/ocare/pose_fuzzy_controller/diff_cmd', Twist)
+    r = rospy.Rate(50) # 50hz
+    while not rospy.is_shutdown():
+        orient = orient + angular_speed + angular_speed_micro
+        twist = Twist()
+        twist.linear.x = linear_speed
+        twist.angular.z = orient + angular_offset
+        pub.publish(twist)
+        r.sleep()
  
-	r = rospy.Rate(10) # 10hz
-	while not rospy.is_shutdown():
-		r.sleep()
- 
-	# spin() simply keeps python from exiting until this node is stopped
-	rospy.spin()
- 
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
 if __name__ == '__main__':
-	joy_teleop()
+    joy_teleop()
